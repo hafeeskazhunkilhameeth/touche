5a6e99ca8e3ce1dfaac7bb0aaef7cf6747165977
Joelios <joel.mesmer@libracore.com>
2018-09-20 10:45:46 +0200

    added function for calculating total time of beratung and some corrections in other doctypes

--- touche/touche/doctype/touche_beratung/touche_beratung.py
+++ touche/touche/doctype/touche_beratung/touche_beratung.py
@@ -7,4 +7,33 @@ import frappe
 from frappe.model.document import Document
 
 class ToucheBeratung(Document):
-	pass
+	def before_save(self):
+		getSummeAufwand(self)
+
+
+def getSummeAufwand(self):
+	time = 0
+	hh = 0
+	mm = 0
+	
+	for entry in self.get("beratungs_timesheet"):
+		time = time + int(entry.dauer_in_min)
+		
+	if time > 60:
+		while time >= 60:
+			hh = hh + 1
+			time = time - 60
+		if time < 10:
+			time = "0" + str(time)
+		mm = time
+	else:
+		if time == 60:
+			hh = 1
+			time = "00"
+		else:
+			hh = 0
+		if time < 10:
+			time = "0" + str(time)
+		mm = time
+		
+	self.summe_aufwand = str(hh) + ":" + str(mm)
