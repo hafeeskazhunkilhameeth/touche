bb2eb76d5dc3bb0c7fe0b068f42eb5cd6ac10692
Joelios <joel.mesmer@libracore.com>
2018-09-20 11:00:25 +0200

    added function to calculate total time of fall

--- touche/touche/doctype/touche_fall/touche_fall.py
+++ touche/touche/doctype/touche_fall/touche_fall.py
@@ -7,4 +7,21 @@ import frappe
 from frappe.model.document import Document
 
 class ToucheFall(Document):
-	pass
+	def onload(self):
+		hh = 0
+		mm = 0
+		query = """SELECT `summe_aufwand` FROM `tabTouche Beratung` WHERE `fall` = '{0}'""".format(self.name)
+		times = frappe.db.sql(query, as_list = True)
+		for time in times:
+			splittet_time = time[0].split(":")
+			hh = hh + int(splittet_time[0])
+			mm = mm + int(splittet_time[1])
+			
+		if mm >= 60:
+			while mm >= 60:
+				hh = hh + 1
+				mm = mm - 60
+		if mm < 10:
+			mm = "0" + str(mm)
+			
+		self.summe_zeitaufwand = str(hh) + ":" + str(mm)
