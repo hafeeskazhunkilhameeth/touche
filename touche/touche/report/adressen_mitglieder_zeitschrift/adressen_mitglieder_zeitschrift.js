3d8db692dec1a7fb07619650ab7a3a9ea8f7b361
Joelios <joel.mesmer@libracore.com>
2019-03-06 10:54:27 +0100

    new report

---
+++ touche/touche/report/adressen_mitglieder_zeitschrift/adressen_mitglieder_zeitschrift.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Adressen Mitglieder Zeitschrift"] = {
+	"filters": [
+
+	]
+}
